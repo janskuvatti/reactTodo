d2d2d1f421dad1034d96b106b8e012b773ac09de
Janina Porkka <janina.porkka@gmail.com>
2019-11-16 10:12:06 +0200

    Update todo.js

--- src/components/todo.js
+++ src/components/todo.js
@@ -22,13 +22,7 @@ import 'react-table/react-table.css';
     </div>
   );
    
- /* <table><tbody><tr><th>Date</th><th>Description</th></tr>{props.todos.map((item, index) => <tr key=   {index}><td>{   item.date}</td><td>{item.desc}</td>
-  <td>     
-<button onClick={() => {props.deleteTodo(index)}}>Delete </button></td>
 
-   </tr>)}</tbody></table>
-                            
-    <br/>*/
 };
 
 
